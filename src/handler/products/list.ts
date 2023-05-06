70aa6ca5d29476e4d82b69d7665f89d1a774e8f2
Peter John R. Rueca <peterrueca@gmail.com>
2023-05-06 21:26:57 +0800

    feat: list products

---
+++ src/handler/products/list.ts
@@ -0,0 +1,59 @@
+import Context from '@/core/context'
+import { Dictionary } from '@/core/types'
+import Joi from 'joi'
+import _ from 'lodash'
+
+const PAGE_LIMIT = 50
+
+const extract = (ctx: Context) => {
+  const data = _.pick(ctx.params, ['lastKey'])
+  const schema = Joi.object()
+    .keys({ lastKey: Joi.string() })
+
+  const result = schema.validate(data)
+
+  if (result.error) {
+    ctx.throw('validation_error', result.error.message)
+  }
+
+  if (data.lastKey) {
+    try {
+      _.assign(result.value, {
+        lastKey: ctx.db.oid(data.lastKey)
+      })
+    } catch (error) {
+      ctx.throw('invalid_id')
+    }
+  }
+
+  return result.value
+}
+
+const list = async (ctx: Context, data: Dictionary) => {
+  const opts = { limit: PAGE_LIMIT }
+  const query = {}
+
+  if (data.lastKey) {
+    _.assign(query, {
+      _id: { $gt: data.lastKey }
+    })
+  }
+
+  const docs = await ctx.db.find('products', query, opts)
+
+  return docs
+}
+
+export default async (ctx: Context) => {
+  const data = extract(ctx)
+  const docs = await list(ctx, data)
+  const response = { products: docs }
+
+  if (docs.length) {
+    _.assign(response, {
+      lastKey: _.get(_.last(docs), ['_id'], null)
+    })
+  }
+
+  ctx.data(response)
+}
--- src/route/products.ts
+++ src/route/products.ts
@@ -4,8 +4,9 @@ import * as products from '@/handler/products'
 
 const router = Router()
 
+router.get('/list/:lastKey?', wrap(products.list))
 router.get('/:productCode', wrap(products.fetch))
-router.post('/', wrap(products.create))
+router.post('/create', wrap(products.create))
 router.post('/update', wrap(products.update))
 router.post('/restock', wrap(products.restock))
 router.post('/archival', wrap(products.archival))
