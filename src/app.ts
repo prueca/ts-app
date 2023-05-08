ae878ba03d8ffbcb5dc7390183bf5c5bb41bae04
Peter John R. Rueca <peterrueca@gmail.com>
2023-05-08 21:36:30 +0800

    use netlify

--- src/app.ts
+++ src/app.ts
@@ -6,16 +6,15 @@ import routes from './route'
 import Context from '@/core/context'
 import { connect } from '@/core/db'
 
-(async () => {
+const app = express()
+
+const start = async () => {
     await connect()
-
-    const app = express()
-    const PORT = process.env.PORT || '8000'
-
     app.use(cors())
     app.use(express.json())
     app.use(Context.attach())
-    app.use(routes)
+    app.use('/.netlify/functions/api', routes)
+}
 
-    app.listen(PORT, () => console.log(`App running on port ${PORT}!`))
-})()
+start()
+export default app
