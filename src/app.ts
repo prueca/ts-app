5251e26a81e1c812bef107e026dca871732ba8db
Peter John R. Rueca <peterrueca@gmail.com>
2023-07-31 21:14:59 +0800

    flag for db conn

--- src/app.ts
+++ src/app.ts
@@ -6,16 +6,20 @@ import Context from '@/core/context'
 import routes from './route'
 import { connect } from '@/core/db'
 
-(async () => {
+const run = async () => {
+  if (!Number(process.env.DB_OFF)) {
     await connect()
+  }
 
-    const app = express()
-    const PORT = process.env.PORT || '8000'
+  const app = express()
+  const PORT = process.env.PORT || '8000'
 
-    app.use(cors())
-    app.use(express.json())
-    app.use(Context.attach())
-    app.use(routes)
+  app.use(cors())
+  app.use(express.json())
+  app.use(Context.attach())
+  app.use(routes)
 
-    app.listen(PORT, () => console.log(`App running on port ${PORT}!`))
-})()
+  app.listen(PORT, () => console.log(`App running on port ${PORT}!`))
+}
+
+run()
